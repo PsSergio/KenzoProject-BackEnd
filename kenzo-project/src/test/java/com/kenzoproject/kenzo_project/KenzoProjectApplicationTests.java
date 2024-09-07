3e5bafff558ab1f2dbd39156c32e6bf6254f4baa
Paulo Sergio <pscosta2008@gmail.com>
2024-09-07 12:12:40 -0300

    feat: adding project structure

---
+++ kenzo-project/src/test/java/com/kenzoproject/kenzo_project/KenzoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenzoproject.kenzo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenzoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
